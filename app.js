bf4c21d16039970a2e96af2971c48684950b64dd
Hoyt W. Dingus <hoyt.dingus@fullsail.edu>
2014-04-17 10:25:14 -0400

    app.js changes

---
+++ app.js
@@ -0,0 +1,15 @@
+//Js file new!!!!!
+console.log("Hell0 W0r1d!!!")
+
+<<<<HEAD
+//THis is a Comment
+===================
+var array = [1,2,3,4];
+if(var i = i; i < array.length; i++){
+
+}
+
+//hi hi hi hi hi hi ho ho ho ho ho
+
+
+console.log(i);
